0b1e847fde2960af7780ce0f7771e13eba8aa192
Бахтияр Башаров <bahtiyarbasharov@gmail.com>
2025-02-06 14:18:17 +0500

    Add project files.

---
+++ WebJob1Delete/View/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows; 
+using System.IO; 
+using WebJob1Delete.ViewModel; 
+using Microsoft.Web.WebView2.Core;
+
+
+namespace WebJob1Delete
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+            Window_Loaded();
+
+            WebViewControl.CoreWebView2InitializationCompleted += WebViewControl_CoreWebView2InitializationCompleted;
+        }
+
+        private void WebViewControl_CoreWebView2InitializationCompleted(object? sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            // Передаем CoreWebView2 в ViewModel
+            if (WebViewControl.CoreWebView2 != null)
+            {
+                var viewModel = (MainViewModel)DataContext;
+                viewModel.InitializeWebView(WebViewControl.CoreWebView2);
+            }
+        }
+
+        private async void Window_Loaded()
+        {
+            #region Поиск файла в проекте
+            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string htmlPath = Path.Combine(appDirectory, "Web", "WebWindow.html");
+
+            if (!Path.Exists(htmlPath))
+            {
+                MessageBox.Show($"Файл не найден: {htmlPath}");
+                return;
+            }
+            #endregion
+
+            WebViewControl.Source = new Uri(htmlPath);
+            await WebViewControl.EnsureCoreWebView2Async();
+        }
+    }
+}
---
+++ WebJob1Delete/ViewModel/MainViewModel.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+
+namespace WebJob1Delete.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _textInWeb = string.Empty;
+        public string TextInWeb
+        {
+            get { return _textInWeb; }
+            set
+            {
+                if (_textInWeb != value)
+                {
+                    _textInWeb = value;
+                    Debug.WriteLine($"TextInWeb изменен: {TextInWeb}");
+                    OnPropertyChanged(nameof(TextInWeb));
+                    SendTextToWpf.RaiseCanExecuteChanged(); // Уведомляем команду о необходимости перепроверки
+                }
+            }
+        }
+
+        private string _textInTextBox = string.Empty;
+        public string TextInTextBox
+        {
+            get { return _textInTextBox; }
+            set
+            {
+                if (_textInTextBox != value)
+                {
+                    _textInTextBox = value;
+                    OnPropertyChanged(nameof(TextInTextBox));
+                }
+            }
+        }
+        private string _textInTextBlock = string.Empty;
+        public string TextInTextBlock
+        {
+            get { return _textInTextBlock; }
+            set
+            {
+                if (_textInTextBlock != value) _textInTextBlock = value;
+                OnPropertyChanged(nameof(TextInTextBlock));
+            }
+        }
+
+        private CoreWebView2? _coreWebView;
+        public CoreWebView2? CoreWebView
+        {
+            get { return _coreWebView; }
+            set
+            {
+                _coreWebView = value;
+                OnPropertyChanged(nameof(CoreWebView));
+            }
+        }
+
+        public MainViewModel()
+        {
+            SendTextToWebView = new RelayCommand(TextToWebView, CanTextToWebView);
+            SendTextToWpf = new RelayCommand(TextToWpf, CanTextToWpf);
+        }
+
+        private bool CanTextToWpf(object? obj)
+        {
+            Debug.WriteLine($"CanExecute? {TextInWeb.Length > 0}");
+            //return !string.IsNullOrEmpty(TextInWeb);
+            return true;
+        }
+
+        private void TextToWpf(object? obj)
+        {
+            TextInTextBlock = TextInWeb;
+        }
+
+        private bool CanTextToWebView(object? arg)
+        {
+            return !string.IsNullOrEmpty(TextInTextBox);
+        }
+
+        private async void TextToWebView(object? obj)
+        {
+            if (CoreWebView != null)
+            {
+                // Используем функцию setTextFromCSharp, определенную в HTML
+                Debug.WriteLine($"Отправляю в WebView2: {TextInTextBox}");
+                string script = $"setTextFromCSharp('{TextInTextBox}');";
+                await CoreWebView.ExecuteScriptAsync(script);
+            }
+        }
+
+        public void InitializeWebView(CoreWebView2 webView)
+        {
+            CoreWebView = webView;
+            CoreWebView.WebMessageReceived += CoreWebView_WebMessageReceived;
+        }
+
+        private void CoreWebView_WebMessageReceived(object? sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            string textFromWeb = e.WebMessageAsJson.Trim('"');
+            TextInWeb = textFromWeb;
+        }
+
+        public RelayCommand SendTextToWebView { get; set; }
+        public RelayCommand SendTextToWpf { get; set; }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
